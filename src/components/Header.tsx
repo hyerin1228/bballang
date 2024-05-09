b1fd05e042d8f17f47a23ab5a21e50c90a52cc65
hyerin1228 <hyerin1228@gmail.com>
2024-05-09 18:25:16 +0900

    style: 헤더 레이아웃 및 스타일 코드 작성

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="sticky top-0 flex items-center bg-white border-b border-gray-300 p-4 h-16">
+      <Link href="/" className="text-lg ml-5">
+        <h1 className="inline-block text-3xl">벌렁</h1>
+      </Link>
+      <nav className="ml-20">
+        <Link href="/" className="text-lg">
+          Brand
+        </Link>
+      </nav>
+      <div className="flex items-center ml-auto gap-4 pr-4">
+        <Link href="/" className="text-lg">
+          <span>장바구니</span>
+        </Link>
+        <Link href="/sign-up" className="text-lg">
+          <span>회원가입</span>
+        </Link>
+        <Link href="/" className="text-lg">
+          <span>로그인</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
