90fe85fb8653e7c3992bcc69665224a71a2d4224
hyerin1228 <hyerin1228@gmail.com>
2024-05-23 18:02:21 +0900

    fix: revalidate 작동하지 않는 듯 해서 코드 수정(테스트 중)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import Link from "next/link";
 SSG가 빌드시에 한 번 호출이 된다는 것 같은데, revalidate 를 통해서 60초에 한 번씩 호출이 되는지 확인을 어떻게 해야할지...??
 */
 
+export const revalidate = 60;
+
 async function getProducts() {
   // fetch('https://...', { next: { revalidate: 3600 } })
   // const response = await axios.get("https://api.ballang.yoojinyoung.com/products/");
@@ -24,11 +26,9 @@ async function getProducts() {
   return data.result;
 }
 
-export const revalidate = 60;
-
 export default async function Page() {
   const products: Product[] = await getProducts();
-  console.log("rendered");
+  console.log("Page rendered at", new Date().toLocaleTimeString());
 
   return (
     <main>
