cfa3a616147e37eb558626ce87a77aa84ccf7f6a
hyerin1228 <hyerin1228@gmail.com>
2024-06-12 21:53:28 +0900

    fix: 상품 상세페이지 ssr 방식으로 데이터 페칭하도록 수정

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,33 @@
-"use client";
+/*
+  * 상품 상세 페이지 - SSR
 
+  1. 데이터 페칭
+    1. ssr 서버 사이드에서 데이터 불러오도록 수정이 필요
+    Q. useParams 를 사용해야하는데.. 서버컴포넌트에서는 사용이 안되는 듯 ..
+  
+  2. 상품의 장바구니 담기
+   1. 로그인이 된 상태
+    - 장바구니에 담기 버튼 클릭 시 
+      - 장바구니에 상품이 담겨야 한다.
+    - 상품이 이미 장바구니에 있는 경우
+      - 장바구니에서 빼기 버튼이 보여져야 한다.
+      - 장바구니에서 빼기 버튼 클릭 시 상품이 장바구니에서 삭제되어야 한다.
+
+    2. 로그인이 안 된 상태
+      - 장바구니에 담기 버튼 클릭 시 
+        - 로그인 모달이 뜨도록 한다. ( 오른쪽 하단에 뜨고 몇 초 후 사라지는 알림창..)
+        - 홈화면으로 이동한다.
+    1. 장바구니에 담기 버튼 클릭 시 로그인 모달이 뜨도록 한다.
+    2. 로그인이 성공하면 오른쪽 하단에 알림창이 뜨네요! 로그인이 성공했습니다(?) 라는 메시지가 뜨다가 몇 초 후 사라지고, 동시에 홈화면으로 이동함.
+  
+  3. 상품의 장바구니 빼기
+    1. 상품 상세 페이지 로드 시
+      - 사용자가 로그인되어 있고 해당 상품이 장바구니에 있는 경우 장바구니 빼기 버튼ㅇ이 보여져야 한다.
+
+*/
+
+import axiosInstance from "@/apis/config";
 import Image from "next/image";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
 interface Product {
   id: number;
@@ -18,23 +43,26 @@ interface Product {
   };
 }
 
-function ProductDetailPage() {
-  const { productId } = useParams();
+async function getProduct(productId: string): Promise<Product> {
+  try {
+    const response = await axiosInstance.get(`/products/${productId}`);
+    console.log("response", response.data.result);
+    return response.data.result;
+  } catch (error) {
+    console.error("Failed to fetch product", error);
+    throw error;
+  }
+}
 
+export default async function ProductDetailPage({
+  params,
+}: {
+  params: { productId: string };
+}) {
+  const { productId } = params;
   console.log("productId", productId);
-  const [product, setProduct] = useState<Product | null>(null);
 
-  useEffect(() => {
-    async function fetchProduct() {
-      const response = await fetch(
-        `https://api.ballang.yoojinyoung.com/products/${productId}`
-      );
-      const data = await response.json();
-      console.log("data", data.result);
-      setProduct(data.result);
-    }
-    fetchProduct();
-  }, [productId]);
+  const product: Product = await getProduct(productId);
 
   return (
     <section className="py-6 px-5 lg:py-10 lg:px-8 max-w-screen-lg mx-auto flex flex-col lg:flex-row items-stretch">
@@ -92,5 +120,3 @@ function ProductDetailPage() {
     </section>
   );
 }
-
-export default ProductDetailPage;
